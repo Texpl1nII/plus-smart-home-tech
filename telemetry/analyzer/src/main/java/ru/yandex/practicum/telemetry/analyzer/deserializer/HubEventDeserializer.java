4745d7f6df7b3f0d33f94781d2a086868fc3e19c
Texpl1nII <Texpl1n@yandex.ru>
2026-02-01 16:16:43 +0500

    правки

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/deserializer/HubEventDeserializer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/deserializer/HubEventDeserializer.java
@@ -1,9 +1,30 @@
 package ru.yandex.practicum.telemetry.analyzer.deserializer;
 
-import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+import ru.yandex.practicum.kafka.telemetry.event.HubEvent;
 
-public class HubEventDeserializer extends BaseAvroDeserializer<HubEventAvro> {
-    public HubEventDeserializer() {
-        super(HubEventAvro.getClassSchema());
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class HubEventDeserializer implements Deserializer<HubEvent> {
+
+    @Override
+    public HubEvent deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data)) {
+            DatumReader<HubEvent> reader = new SpecificDatumReader<>(HubEvent.class);
+            Decoder decoder = DecoderFactory.get().binaryDecoder(inputStream, null);
+            return reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new SerializationException("Error deserializing HubEvent", e);
+        }
     }
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/deserializer/SensorsSnapshotDeserializer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/deserializer/SensorsSnapshotDeserializer.java
@@ -1,9 +1,30 @@
 package ru.yandex.practicum.telemetry.analyzer.deserializer;
 
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshot;
 
-public class SensorsSnapshotDeserializer extends BaseAvroDeserializer<SensorsSnapshotAvro> {
-    public SensorsSnapshotDeserializer() {
-        super(SensorsSnapshotAvro.getClassSchema());
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class SensorsSnapshotDeserializer implements Deserializer<SensorsSnapshot> {
+
+    @Override
+    public SensorsSnapshot deserialize(String topic, byte[] data) {
+        if (data == null) {
+            return null;
+        }
+
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data)) {
+            DatumReader<SensorsSnapshot> reader = new SpecificDatumReader<>(SensorsSnapshot.class);
+            Decoder decoder = DecoderFactory.get().binaryDecoder(inputStream, null);
+            return reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new SerializationException("Error deserializing SensorsSnapshot", e);
+        }
     }
 }
