635d4bbdeb403abeb657f7d5f27a4bcba2415ad2
Texpl1nII <Texpl1n@yandex.ru>
2026-02-02 15:29:34 +0500

    правки

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/repository/ScenarioRepository.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/telemetry/analyzer/repository/ScenarioRepository.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.telemetry.analyzer.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.telemetry.analyzer.model.Scenario;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ScenarioRepository extends JpaRepository<Scenario, Long> {
     List<Scenario> findByHubId(String hubId);
     Optional<Scenario> findByHubIdAndName(String hubId, String name);
