1211df241c7402a4f642bcbf32ac40a8013ac12a
Texpl1nII <Texpl1n@yandex.ru>
2026-02-04 19:29:41 +0500

    правки

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/exception/DeserializationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.telemetry.aggregator.exception;
-
-public class DeserializationException extends RuntimeException {
-    public DeserializationException(String message, Throwable ex) {
-        super(message, ex);
-    }
-}
