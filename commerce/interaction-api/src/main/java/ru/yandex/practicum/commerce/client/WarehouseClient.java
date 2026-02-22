1e7546b88f15a5d221ddaa67bea04f3c43ff699d
Texpl1nII <Texpl1n@yandex.ru>
2026-02-22 22:43:44 +0500

    правки

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/client/WarehouseClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/commerce/client/WarehouseClient.java
@@ -2,10 +2,9 @@ package ru.yandex.practicum.commerce.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.commerce.cart.config.FeignConfig;
 import ru.yandex.practicum.commerce.dto.*;
 
-@FeignClient(name = "WAREHOUSE", configuration = FeignConfig.class)
+@FeignClient(name = "warehouse", url = "${warehouse.url:}")
 public interface WarehouseClient {
 
     @PostMapping("/api/v1/warehouse/check")
